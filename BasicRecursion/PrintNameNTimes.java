c74ac6d59acf147dba60807feeb96956d67b30ab
Tanuj Pratap Singh <tanujsinghthakur9999@gmail.com>
2026-06-11 12:30:45 +0530

    Print name N time using recursion

---
+++ BasicRecursion/PrintNameNTimes.java
@@ -0,0 +1,16 @@
+public class PrintNameNTimes {
+
+    static void printName(int i, int n) {
+        if(i > n){
+            return;
+        }
+        System.out.println("Tanuj");
+        i++;
+        printName(i, n);
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        printName(1, n);
+    }
+}
