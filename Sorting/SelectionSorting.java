7eb94b150c3a742778ac32cf41798cbb535c739d
Tanuj Pratap Singh <tanujsinghthakur9999@gmail.com>
2026-06-12 15:25:28 +0530

    Selection Sort

--- Sorting/SelectionSorting.java
+++ Sorting/SelectionSorting.java
@@ -1,19 +1,28 @@
 public class SelectionSorting {
-    public static void main(String[] args) {
-        int[] arr = { 13, 46, 24, 52, 20, 9 };
-        int n = arr.length;
+
+    static void selectionSort(int[] a) {
+        int n = a.length;
         for (int i = 0; i <= n - 2; i++) {
             int minIndex = i;
             for (int j = i; j <= n - 1; j++) {
-                if (arr[j] < arr[minIndex]) {
+                if (a[j] < a[minIndex]) {
                     minIndex = j;
                 }
             }
-            int temp = arr[i];
-            arr[i] = arr[minIndex];
-            arr[minIndex] = temp;
+            int temp = a[i];
+            a[i] = a[minIndex];
+            a[minIndex] = temp;
         }
-        System.out.println("Sorted array is :");
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 13, 46, 24, 52, 20, 9 };
+        System.out.println("Unsorted array is :");
+        for (int element : arr) {
+            System.out.print(element + " ");
+        }
+        selectionSort(arr);
+        System.out.println("\nSorted array is :");
         for (int element : arr) {
             System.out.print(element + " ");
         }
