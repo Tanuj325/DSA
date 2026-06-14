a0b373c6d2c0829da5e63fdfe945cb3c9ea3a405
Tanuj Pratap Singh <tanujsinghthakur9999@gmail.com>
2026-06-14 11:00:45 +0530

    Second Largest Element in array

---
+++ Array/SecondLargest.java
@@ -0,0 +1,22 @@
+public class SecondLargest {
+    static int getSecondLargest(int[] arr) {
+		int largest = arr[0];
+		int secondLargest = -1;
+		
+		for (int i = 0; i<arr.length; i++) {
+			if(arr[i]>largest){
+			    secondLargest = largest;
+			    largest = arr[i];
+			}
+			else if (arr[i] < largest && arr[i] > secondLargest){
+			    secondLargest = arr[i];
+			}
+		}
+		return secondLargest;
+	}
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,4,7,7,5};
+        System.out.println("Second Largest Element is : " + getSecondLargest(arr));
+    }
+}
