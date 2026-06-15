36a62342ee9aa01a367324612c4c4ee65acf6e38
Tanuj Pratap Singh <tanujsinghthakur9999@gmail.com>
2026-06-15 11:27:35 +0530

    Intersection of two sorted array

---
+++ Array/IntersectionOfTwoSortedArray.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class IntersectionOfTwoSortedArray {
+    public static void main(String[] args) {
+        int[] a = { 1, 2, 2, 3, 3, 4, 5, 6 };
+        int[] b = { 2, 3, 3, 5, 6, 6, 7 };
+        int n1 = a.length;
+        int n2 = b.length;
+        int i = 0;
+        int j = 0;
+
+        List<Integer> intersection = new ArrayList<>();
+
+        while (i < n1 && j < n2) {
+            if (a[i] == b[j]) {
+                intersection.add(a[i]);
+                i++;
+                j++;
+            } else {
+                i++;
+            }
+        }
+
+        for (int element : intersection) {
+            System.out.print(element + " ");
+        }
+    }
+
+}
