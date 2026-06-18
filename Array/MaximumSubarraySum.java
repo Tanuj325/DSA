2ce4d91199e795456211dd190ee8179f08ea1cdd
Tanuj Pratap Singh <tanujsinghthakur9999@gmail.com>
2026-06-18 11:17:22 +0530

    Find maximum subarray sum using kadane algorithm

--- Array/MaximumSubarraySum.java
+++ Array/MaximumSubarraySum.java
@@ -17,12 +17,43 @@ public class MaximumSubarraySum {
             }
 
         }
+
+        // if someone to print the subarray
+
+        // int startIndex = -1;
+        // int endIndex = -1;
+        // int start = 0;
+        // for (int i = 0; i < n; i++) {
+
+        //     if(sum == 0){
+        //         start =i;
+        //     }
+
+        //     sum += nums[i];
+
+        //     if (sum > max) {
+        //         max = sum;
+        //         startIndex = start;
+        //         endIndex = i;
+        //     }
+
+        //     if (sum < 0) {
+        //         sum = 0;
+        //     }
+
+        // }
+        // System.out.print("Subarray is : ");
+        // for(int i = startIndex;i<=endIndex;i++){
+        //     System.out.print(nums[i]+" ");
+        // }
+
+
         return max;
     }
 
     public static void main(String[] args) {
         int[] nums = { -2, -3, 4, -1, -2, 1, 5, -3 };
-        System.out.println("Maximum Subarray sum using kadane algorithm is : " + maxSubArray(nums));
+        System.out.println("\nMaximum Subarray sum using kadane algorithm is : " + maxSubArray(nums));
     }
 
 }
